4c5bc2318a1301ca694f8d00d44b0c505feebe3b
ductrun309 <141017258+ductrun309@users.noreply.github.com>
2024-01-19 22:07:00 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,315 +1,261 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  const window_width = (canvas.width = window.innerWidth);
-  const window_height = (canvas.height = window.innerHeight);
-  let animation_run = [];
-  let animation_idle = [];
-  let animation_jump = [];
-  let animation_fall = [];
-  let animation_die = [];
-  let animation_dizzy = [];
-  let animation_gethit = [];
-  let animation_attack1 = [];
-  let animation_attack2 = [];
-  let animation_attack3 = [];
-  for (var i = 0; i < 21; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/11_attack_c/skeleton-11_attack_c_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/11_attack_c/skeleton-11_attack_c_${i}.png`;
-    }
-    animation_attack3.push(image);
+const canvas_height = (canvas.height = window.innerHeight);
+const canvas_width = (canvas.width = window.innerWidth);
+
+class Player {
+  constructor() {
+    this.vel = {
+      x: 0,
+      y: 0,
+    };
+    this.rotate = 0;
+    const img = new Image();
+    img.src = "../Transparent PNG/rocket.png";
+    img.onload = () => {
+      this.scale = 0.035;
+      this.width = img.width * this.scale;
+      this.height = img.height * this.scale;
+      this.img = img;
+      this.pos = {
+        x: canvas_width / 2 - this.width / 2,
+        y: (canvas_height * 3) / 4,
+      };
+    };
   }
-  for (var i = 0; i < 16; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/10_attack_b/skeleton-10_attack_b_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/10_attack_b/skeleton-10_attack_b_${i}.png`;
+
+  draw() {
+    ctx.save();
+    ctx.translate(
+      player.pos.x + player.width / 2,
+      player.pos.y + player.height / 2
+    );
+    ctx.rotate(this.rotate);
+    ctx.translate(
+      -player.pos.x - player.width / 2,
+      -player.pos.y - player.height / 2
+    );
+    if (this.img) {
+      ctx.drawImage(this.img, this.pos.x, this.pos.y, this.width, this.height);
     }
-    animation_attack2.push(image);
+    ctx.restore();
   }
-  for (var i = 0; i < 16; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/09_attack_a/skeleton-09_attack_a_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/09_attack_a/skeleton-09_attack_a_${i}.png`;
+  update() {
+    if (this.img) {
+      this.draw();
+      this.pos.x += this.vel.x * 3;
+      this.pos.y += this.vel.y * 3;
     }
-    animation_attack1.push(image);
   }
-  for (var i = 0; i < 11; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/08_get_hit/skeleton-08_get_hit_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/08_get_hit/skeleton-08_get_hit_${i}.png`;
-    }
-    animation_gethit.push(image);
+}
+
+class Bullet {
+  constructor({ pos, vel }) {
+    this.pos = pos;
+    this.vel = vel;
+    this.radius = 3;
   }
-  for (var i = 0; i < 21; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/07_dizzy/skeleton-07_dizzy_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/07_dizzy/skeleton-07_dizzy_${i}.png`;
-    }
-    animation_dizzy.push(image);
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
   }
-  for (var i = 0; i < 31; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/06_ko/skeleton-06_KO_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/06_ko/skeleton-06_KO_${i}.png`;
-    }
-    animation_die.push(image);
+  update() {
+    this.draw();
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
-  for (var i = 0; i < 11; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/05_fall/skeleton-05_fall_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/05_fall/skeleton-05_fall_${i}.png`;
-    }
-    animation_fall.push(image);
+}
+
+class Enemy {
+  constructor({ pos, vel }) {
+    const img = new Image();
+    img.src = "../Transparent PNG/05_fall/skeleton-05_fall_00.png";
+    this.scale = 0.05;
+    this.vel = vel;
+    this.pos = pos;
+    this.health = 3;
+    img.onload = () => {
+      this.scale = 0.05;
+      this.width = img.width * this.scale;
+      this.height = img.height * this.scale;
+      this.img = img;
+    };
   }
-  for (var i = 0; i < 11; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/04_jump/skeleton-04_jump_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/04_jump/skeleton-04_jump_${i}.png`;
+  draw() {
+    if (this.img) {
+      ctx.drawImage(this.img, this.pos.x, this.pos.y, this.width, this.height);
     }
-    animation_jump.push(image);
   }
-  for (var i = 0; i < 21; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/01_idle_a/skeleton-01_idle_a_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/01_idle_a/skeleton-01_idle_a_${i}.png`;
-    }
-    animation_idle.push(image);
+  update() {
+    this.draw();
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
-  for (var i = 0; i < 13; i++) {
-    var image = new Image();
-    if (i < 10) {
-      image.src = `/robot/Transparent PNG/03_run/skeleton-03_run_0${i}.png`;
-    } else {
-      image.src = `/robot/Transparent PNG/03_run/skeleton-03_run_${i}.png`;
-    }
-    animation_run.push(image);
+}
+
+var animation;
+const player = new Player();
+const bullet = [];
+const enemy = [];
+
+let spawn_enemy = setInterval(() => {
+  enemy.push(
+    new Enemy({
+      pos: {
+        x: Math.random() * canvas_width,
+        y: 100,
+      },
+      vel: {
+        x: 1,
+        y: 2,
+      },
+    })
+  );
+}, 1000);
+const key_press = {
+  a: {
+    press: false,
+  },
+  s: {
+    press: false,
+  },
+  d: {
+    press: false,
+  },
+  w: {
+    press: false,
+  },
+  space: {
+    press: false,
+  },
+};
+function key_control() {
+  if (key_press.a.press && player.pos.x > 0) {
+    player.vel.x = -3;
+    player.rotate = -0.1;
+  } else if (key_press.d.press && player.pos.x + player.width < canvas_width) {
+    player.vel.x = 3;
+    player.rotate = 0.1;
+  } else {
+    player.vel.x = 0;
+    player.rotate = 0;
   }
-  class robot {
-    constructor() {
-      this.xpos = Math.random() * window_width;
-      this.ypos = Math.random() * window_height - 50;
-      this.width = 150;
-      this.height = 100;
-      this.sx = Math.random() * 1 + 1;
-      this.sy = Math.random() * 2;
-      this.count = 1;
+  if (key_press.w.press && player.pos.y > 0) {
+    player.vel.y = -3;
+  } else if (
+    key_press.s.press &&
+    player.pos.y + player.height < canvas_height
+  ) {
+    player.vel.y = 3;
+  } else {
+    player.vel.y = 0;
+  }
+}
+function check() {
+  enemy.forEach((ghost) => {
+    if (
+      player.pos.x <= ghost.pos.x + ghost.width &&
+      player.pos.x + player.width >= ghost.pos.x &&
+      player.pos.y <= ghost.pos.y + ghost.height &&
+      player.pos.y + player.height >= ghost.pos.y
+    ) {
+      cancelAnimationFrame(animation);
     }
-    update_run() {
-      this.xpos += this.sx;
-    }
-    run() {
-      if (this.count >= animation_run.length) this.count = 0;
-      ctx.drawImage(
-        animation_run[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-      
-    }
-    idle() {
-      if (this.count >= animation_idle.length) this.count = 0;
-      ctx.drawImage(
-        animation_idle[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    jump() {
-      if (this.count >= animation_die.length){
-        pause();
+  });
+  bullet.forEach((projectile, index) => {
+    enemy.forEach((ghost) => {
+      if (
+        projectile.pos.x - projectile.radius <= ghost.pos.x + ghost.width &&
+        projectile.pos.x + projectile.radius >= ghost.pos.x &&
+        projectile.pos.y - projectile.radius <= ghost.pos.y + ghost.height
+      ) {
+        bullet.splice(index, 1);
+        ghost.health--;
+        console.log("true");
       }
-      ctx.drawImage(
-        animation_jump[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
+    });
+  });
+}
+function animate() {
+  animation = requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas_width, canvas_height);
+  player.update();
+  key_control();
+  bullet.forEach((projectile, index) => {
+    if (projectile.radius + projectile.pos.y <= 0) {
+      bullet.splice(index, 1);
     }
-    fall() {
-      if (this.count >= animation_fall.length) this.count = 0;
-      ctx.drawImage(
-        animation_fall[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
+    projectile.update();
+  });
+  enemy.forEach((ghost, index) => {
+    if (
+      ghost.health == 0 ||
+      ghost.pos.x >= canvas_width ||
+      ghost.pos.y >= canvas_height
+    ) {
+      enemy.splice(index, 1);
     }
-    die() {
-      if (this.count >= animation_die.length){
-        pause();
+    ghost.update();
+  });
+
+  check();
+}
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      key_press.a.press = true;
+      break;
+    case "d":
+      key_press.d.press = true;
+      break;
+    case "w":
+      key_press.w.press = true;
+      break;
+    case "s":
+      key_press.s.press = true;
+      break;
+    case " ":
+      if (!key_press.space.press) {
+        bullet.push(
+          new Bullet({
+            pos: {
+              x: player.pos.x + player.width / 2,
+              y: player.pos.y,
+            },
+            vel: {
+              x: 0,
+              y: -5,
+            },
+          })
+        );
       }
-      ctx.drawImage(
-        animation_die[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    dizzy() {
-      if (this.count >= animation_dizzy.length) this.count = 0;
-      ctx.drawImage(
-        animation_dizzy[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    gethit() {
-      if (this.count >= animation_gethit.length) this.count = 0;
-      ctx.drawImage(
-        animation_gethit[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    attack1() {
-      if (this.count >= animation_attack1.length) this.count = 0;
-      ctx.drawImage(
-        animation_attack1[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    attack2() {
-      if (this.count >= animation_attack2.length) this.count = 0;
-      ctx.drawImage(
-        animation_attack2[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
-    attack3() {
-      if (this.count >= animation_attack3.length) this.count = 0;
-      ctx.drawImage(
-        animation_attack3[this.count],
-        this.xpos,
-        this.ypos,
-        this.width,
-        this.height
-      );
-      this.count++;
-    }
+      key_press.space.press = true;
+      break;
   }
-  var b;
-  let all_robot = [];
-  for (var i = 0; i < 100; i++) {
-    all_robot.push(new robot());
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "a":
+      key_press.a.press = false;
+      break;
+    case "d":
+      key_press.d.press = false;
+      break;
+    case "w":
+      key_press.w.press = false;
+      break;
+    case "s":
+      key_press.s.press = false;
+      break;
+    case " ":
+      key_press.space.press = false;
+      break;
   }
-  function run() {
-    b = requestAnimationFrame(run);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.run();
-      robot.update_run();
-    });
-  }
-  function idle() {
-    b = requestAnimationFrame(idle);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.idle();
-    });
-  }
-  function jump() {
-    b = requestAnimationFrame(jump);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.jump();
-    });
-  }
-  function fall() {
-    b = requestAnimationFrame(fall);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.fall();
-      robot.ypos += 4;
-      if (robot.ypos > window_height) robot.ypos = 0;
-    });
-  }
-  function die() {
-    b = requestAnimationFrame(die);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.die();
-    });
-  }
-  function dizzy() {
-    b = requestAnimationFrame(dizzy);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.dizzy();
-    });
-  }
-  function gethit() {
-    b = requestAnimationFrame(gethit);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.gethit();
-    });
-  }
-  function attack1() {
-    b = requestAnimationFrame(attack1);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.attack1();
-    });
-  }
-  function attack2() {
-    b = requestAnimationFrame(attack2);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.attack2();
-    });
-  }
-  function attack3() {
-    b = requestAnimationFrame(attack3);
-    ctx.clearRect(0, 0, window_width, window_height);
-    all_robot.forEach((robot) => {
-      robot.attack3();
-    });
-  }
-  function pause(){
-    cancelAnimationFrame(b);
-  }
+});
+
+animate();
